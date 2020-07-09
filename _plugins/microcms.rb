2e475fbee64293031ed17f20e407e60fbc66912a
staff-kaihojpn <staff.kaihojpn@gmail.com>
2020-07-09 14:58:28 +0900

    test print action env

--- _plugins/microcms.rb
+++ _plugins/microcms.rb
@@ -3,17 +3,10 @@ require 'open-uri'
 module MicroCMS
   class Generator < Jekyll::Generator
     def generate(site)
+      p('MICROCMS_KEY')
+      
       p(site.env.MICROCMS_KEY)
-      =begin
-      microcms_key = jekyll.environment == "production" ? secrets.MICROCMS_KEY : process.env.MICROCMS_KEY
-      # コンテンツを取得
-      result = JSON.load(open("https://kaihojpn.microcms.io/api/v1/photo",
-        "X-API-KEY" => ""
-      ).read)
-
-      # レイアウト側で使えるようにsite.dataに値を入れておく
-      site.data.merge!({ "photos" => result['contents'] })
-      =end
+      
     end
   end
 end
