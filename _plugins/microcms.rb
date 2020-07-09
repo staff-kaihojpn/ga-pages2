d0aa1cf36566114f389a7b3e016019f4298cd136
staff-kaihojpn <staff.kaihojpn@gmail.com>
2020-07-09 19:23:18 +0900

    cover image recent post

--- _plugins/microcms.rb
+++ _plugins/microcms.rb
@@ -3,7 +3,6 @@ require 'open-uri'
 module MicroCMS
   class Generator < Jekyll::Generator
     def generate(site)
-      pp(ENV)
       # コンテンツを取得
       result = JSON.load(open(site.config['microcms_api'],
         "X-API-KEY" => ENV["MICROCMS_KEY"]
@@ -11,6 +10,10 @@ module MicroCMS
 
       # レイアウト側で使えるようにsite.dataに値を入れておく
       site.data.merge!({ "photos" => result['contents'] })
+
+      # 表紙を設定
+      cover_image = result['contents'][0]['image']['url']
+      site.config['accent_image']=cover_image if cover_image
     end
   end
 end
